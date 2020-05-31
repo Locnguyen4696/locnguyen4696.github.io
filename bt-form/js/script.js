9958ac2febdcb13713ff3fc8829735202d798e11
Loc <loc@Locs-MacBook-Pro.local>
2020-05-31 21:53:34 +0700

    fix form

--- bt-form/js/script.js
+++ bt-form/js/script.js
@@ -1,7 +1,35 @@
-function showValue() {
-  if ($("#password")[0].type == "text") {
-    $("#password")[0].type = "password";
-  } else {
-    $("#password")[0].type = "text";
-  }
+let user={
+  name:"",
+  bday:[],
+  sex:"",
+  address:"",
+  tel:"",
+  email:""
+}
+let day = document.getElementById('day');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+for(let i=1;i<32;i++){
+  let option=document.createElement('option');
+  option.text=i;
+  day.add(option);
+}
+for(let i=1;i<13;i++){
+  let option=document.createElement('option');
+  option.text=i;
+  month.add(option);
+}
+for(let i=1960;i<2021;i++){
+  let option=document.createElement('option');
+  option.text=i;
+  year.add(option);
+}
+function register(){
+
+  user.name=document.getElementById('name').value;
+  user.bday=[day=document.getElementById('day').value,document.getElementById('month').value,document.getElementById('year').value];
+  user.sex=document.getElementById('sex').value;
+  user.address=document.getElementById('address').value;
+  user.tel=document.getElementById('tel').value;
+  user.email=document.getElementById('email').value;
 }
